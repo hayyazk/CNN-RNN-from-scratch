85515607d6eb3d1bfb4257e6c3367ee0ad221c66
hayyazk <130911777+hayyazk@users.noreply.github.com>
2025-05-29 23:36:00 +0700

    fix: use both uni and bi layers in final model (rnn)

--- src/rnn/rnn.py
+++ src/rnn/rnn.py
@@ -1,15 +1,31 @@
 import numpy as np
 
-def rnn_unidirectional(x, W_x, W_h, b):
-    batch_size, time_steps, input_dim = x.shape
+def rnn_unidirectional(x, W_x, W_h, b, return_sequences=False):
+    batch_size, time_steps, _ = x.shape
     hidden_dim = W_h.shape[0]
     h = np.zeros((batch_size, hidden_dim))
+
+    outputs = []
     
     for t in range(time_steps):
         h = np.tanh(np.dot(x[:, t, :], W_x) + np.dot(h, W_h) + b)
-    return h
+        outputs.append(h)
+    
+    if return_sequences:
+        return np.stack(outputs, axis=1)
+    else:
+        return h
 
-def rnn_bidirectional(x, W_x_f, W_h_f, b_f, W_x_b, W_h_b, b_b):
-    h_forward = rnn_unidirectional(x, W_x_f, W_h_f, b_f)
-    h_backward = rnn_unidirectional(x[:, ::-1, :], W_x_b, W_h_b, b_b)
-    return np.concatenate([h_forward, h_backward], axis=-1)
+def rnn_bidirectional(x, W_x_f, W_h_f, b_f, W_x_b, W_h_b, b_b, return_sequences=False):
+    h_f = rnn_unidirectional(x, W_x_f, W_h_f, b_f, return_sequences)
+    h_b = rnn_unidirectional(x[:, ::-1, :], W_x_b, W_h_b, b_b, return_sequences)
+
+    return np.concatenate([h_f, h_b], axis=-1)
+
+    # h_b = h_b[:, ::-1, :]
+    # h_concat = np.concatenate([h_f, h_b], axis=-1)
+
+    # if return_sequences:
+    #     return h_concat
+    # else:
+    #     return h_concat[:, -1, :]  
